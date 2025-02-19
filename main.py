ebce91014c6166dd5aed99cd4fdd3d9437ca0318
Admin <ttttt@gmail.com>
2025-02-19 11:01:55 +1000

    изменение размеров

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ SCREEN_SIZE = [600, 450]
 class Example(QWidget):
     def __init__(self):
         super().__init__()
+        self.spn = 0.01
         self.ll = [37.530887, 55.703118]
         self.getImage()
         self.initUI()
@@ -20,8 +21,7 @@ class Example(QWidget):
         server_address = 'https://static-maps.yandex.ru/v1?'
         api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
         ll = f'll={str(self.ll[0])},{str(self.ll[1])}'
-        spn = 'spn=0.002,0.002'
-        # Готовим запрос.
+        spn = f'spn={self.spn},{self.spn}'
 
         map_request = f"{server_address}{ll}&{spn}&apikey={api_key}"
         response = requests.get(map_request)
@@ -32,7 +32,6 @@ class Example(QWidget):
             print("Http статус:", response.status_code, "(", response.reason, ")")
             sys.exit(1)
 
-        # Запишем полученное изображение в файл.
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
             file.write(response.content)
@@ -40,8 +39,6 @@ class Example(QWidget):
     def initUI(self):
         self.setGeometry(100, 100, *SCREEN_SIZE)
         self.setWindowTitle('Отображение карты')
-
-        ## Изображение
         self.pixmap = QPixmap(self.map_file)
         self.image = QLabel(self)
         self.image.move(0, 0)
@@ -57,6 +54,11 @@ class Example(QWidget):
         if key:
             try:
                 print(type(key), int(Qt.Key.Key_Down))
+                if key == Qt.Key.Key_PageUp:
+                    self.spn += 0.01
+                if key == Qt.Key.Key_PageDown:
+                    if self.spn - 0.01 > 0:
+                        self.spn -= 0.01
                 if key == int(Qt.Key.Key_Down):
                     self.ll[1] -= 0.0002
                 # if key == Qt.Key.Key_left:
@@ -72,6 +74,7 @@ class Example(QWidget):
                 self.image.update()
             except Exception as e:
                 print(e)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
